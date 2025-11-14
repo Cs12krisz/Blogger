f2ec750add86979d6430df250b8545791e617dee
Cs12krisz <csigerkrisztian@gmail.com>
2025-11-14 10:01:24 +0100

    Külön a context fájl a Context mappában

---
+++ Blogger/Contexts/BloggerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Blogger.Models;
+
+namespace Blogger.Contexts
+{
+    public class BloggerDbContext : DbContext
+    {
+        public BloggerDbContext() { }
+
+        public BloggerDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Models.Blogger> Blog { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=blog;user=root;password='';");
+        }
+    }
+}
